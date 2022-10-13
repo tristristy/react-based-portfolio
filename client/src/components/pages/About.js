db69258b7fdeb5b25a7a32ba80498a4c270ae545
tristristy <triswilbs@gmail.com>
2022-10-13 10:46:34 -0400

    edited about section

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <Card style={{ width: "25rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Body>
+          <Card.Title>Tris Wilbraham</Card.Title>
+          <Card.Text>
+            Hello, my name is Tris and I am a junior web developer. I enjoy all things nerdy such as Dungeons and Dragons
+            as well things relating to Video Games. I lean more towards the back-end of development, enjoying creating database stuctures
+            as well as assigning relations between tables.
+          </Card.Text>
+        </Card.Body>
+        <Card.Body>
+          <Card.Link href="#">Github</Card.Link>
+          <Card.Link href="#">LinkedIn</Card.Link>
+          <Card.Link href="#">Email</Card.Link>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
